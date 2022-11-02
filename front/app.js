e0e24314c741346d326fc6144424e8fd8b7408cb
aymeric s <sandoz.aymeric@gmail.com>
2022-11-02 12:47:22 +0100

    modif pour vercel

--- front/app.js
+++ front/app.js
@@ -49,7 +49,7 @@ document.getElementById("switch").addEventListener("change", function () {
 });
 
 const fetchCardList = () => {
-  fetch(`/api/card/`)
+  fetch(`http://localhost:5000/api/card/`)
     .then((reponse) => reponse.json())
     .then((allSpikemmon) => {
       console.log("allspikemon:", allSpikemmon);
@@ -114,7 +114,7 @@ setTimeout(function () {
 // };
 //////////Get your own cards
 const fetchOwnerCardList = () => {
-  fetch("/api/card/ownerCards", {
+  fetch("http://localhost:5000/api/card/ownerCards", {
     method: "GET",
     headers: {
       authorization: `Bearer ${localStorage.getItem("token")}`,
